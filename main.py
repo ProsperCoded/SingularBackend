883efebab49cd1530677e045daaf07d1548788ad
Hermes <hermeschigoziri@gmail.com>
2026-05-13 12:39:48 +0100

    hook up router for verification endpoints

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from api.webhooks import router as webhooks_router
 from api.vendors import router as vendors_router
 from api.schemas_preview import router as schemas_preview_router
 from api.batches import router as batches_router
+from api.verify import router as verify_router
 
 app = FastAPI()
 
@@ -18,5 +19,6 @@ api_router.include_router(webhooks_router)
 api_router.include_router(vendors_router)
 api_router.include_router(schemas_preview_router)
 api_router.include_router(batches_router)
+api_router.include_router(verify_router)
 
 app.include_router(api_router)
