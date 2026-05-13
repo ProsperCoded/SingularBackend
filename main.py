80ca093a43f783ef6d354d0f271bc01c68367d59
Hermes <hermeschigoziri@gmail.com>
2026-05-13 21:30:59 +0100

    remove schemas_preview endpoints

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, APIRouter
 from api.webhooks import router as webhooks_router
 from api.vendors import router as vendors_router
-from api.schemas_preview import router as schemas_preview_router
 from api.batches import router as batches_router
 from api.verify import router as verify_router
 from api.analytics import router as analytics_router
@@ -10,15 +9,8 @@ app = FastAPI()
 
 api_router = APIRouter(prefix="/api")
 
-
-@api_router.get("/")
-def hello():
-    return {"Hello": "World"}
-
-
 api_router.include_router(webhooks_router)
 api_router.include_router(vendors_router)
-api_router.include_router(schemas_preview_router)
 api_router.include_router(batches_router)
 api_router.include_router(verify_router)
 api_router.include_router(analytics_router)
